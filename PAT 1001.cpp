f9d8ac2647c22333d946b96b89094c362bbd97c1
Liang Quan <ruthlqtt@163.com>
2014-09-04 13:01:28 +0800

    Create PAT 1001.cpp

---
+++ PAT 1001.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    a = a + b;
+    int sign=1;
+    if(a==0){
+        cout<<a<<endl;
+        return 0;
+    }else if(a<0)
+        sign=-1;
+    string str;
+    str = to_string(static_cast<long long>(a*sign));
+    reverse(str.begin(), str.end());
+    int i = 0, limit = (str.size()-1)/3;
+    cout<<"init finished"<<endl;
+    while(i<limit){
+        int pos = (i+1)*3 + i-1;
+        str.insert(pos, 1, ',');
+        ++i;
+    }
+    reverse(str.begin(), str.end());
+    if(sign == -1)
+        str.insert(0 ,1, '-');
+    cout<<str<<endl;
+    return 0;
+}
